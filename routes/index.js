06a624a5e9aa2d0b1efa912827081d8fe87d717f
Barbara Diaz <barbarad91@gmail.com>
2021-02-08 18:46:41 +0100

    Add boss routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-module.exports = app => {
-	// Base URLS
-	app.use('/', require('./base.routes.js'))
-	app.use('/', require('./auth.routes.js'))
+module.exports = (app) => {
+  // Base URLS
+  app.use("/", require("./base.routes.js"))
+  app.use("/", require("./auth.routes.js"))
+  app.use("/", require("./boss.routes.js"))
 }
