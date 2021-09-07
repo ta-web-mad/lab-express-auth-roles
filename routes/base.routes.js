c5cee598dc8a6a5f1b5bb70bcff4cc1ceb8f5543
jose ramon <rabohopo@gmail.com>
2021-09-07 09:18:40 +0200

    done

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,7 +1,92 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const User = require("./../models/User.model");
+const { isLoggedIn, checkRole, checkValue } = require("../middleware");
 
 router.get("/", (req, res, next) => {
-  res.render("index")
-})
+  res.render("index");
+});
 
-module.exports = router
+router.get("/students", isLoggedIn, (req, res) => {
+  // res.send('Hola soy un estudiante')
+
+  User.find({ role: "STUDENT" }) // renderiza contenido tanto por inicio de sesion (isLogged) como por rol (isPM)
+    .then((users) => {
+      console.log(users);
+      res.render("users/user-list", { users });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/students/:id", (req, res) => {
+  const { id } = req.params;
+
+  User.findById(id) // renderiza contenido tanto por inicio de sesion (isLogged) como por rol (isPM)
+    .then((user) => {
+      console.log(user);
+      res.render("users/user-details", {
+        user,
+        isPM: req.session.currentUser?.role === "PM",
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/students/delete/:id", (req, res) => {
+  const { id } = req.params;
+
+  User.findByIdAndDelete(id)
+    .then((removedUser) => {
+      res.redirect("/students");
+      console.log("El Usuario eliminado es:", removedUser);
+    })
+    .catch((err) => console.log("Hubo un error:", err));
+});
+
+router.get("/students/edit/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("esto es el id", id);
+  User.findById(id).then((user) => {
+    console.log("esto es user", user);
+    res.render("users/user-edit", user);
+  });
+});
+
+router.post("/students/edit/:id", checkRole("PM"), (req, res) => {
+  const { id } = req.params;
+  const { username, role } = req.body;
+
+  
+
+  User.findByIdAndUpdate(id, { username: username, role: role }, { new: true })
+    .then((User) => {
+      res.redirect("/students");
+      console.log("El usuario ha sido modificado:", User);
+    })
+    .catch((err) => console.log("Hubo un error", err));
+});
+
+router.get("/students/userEdit/:id", checkValue, (req, res) => {
+  
+  const { id } = req.params;
+  console.log("esto es el id", id);
+  User.findById(id).then((user) => {
+   
+    res.render("users/user-edit", {user, isPM: req.session.currentUser?.role === 'PM'});
+  });
+
+});
+
+router.post("/students/userEdit/:id", checkValue, (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  const { username} = req.body;
+
+  User.findByIdAndUpdate(id, {username: username}, { new: true })
+    .then((User) => {
+      res.redirect("/students");
+      console.log("El usuario ha sido modificado:", User);
+    })
+    .catch((err) => console.log("Hubo un error", err));
+});
+
+module.exports = router;
