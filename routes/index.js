da74794eecb79227dfef3b2ae7557b209e9e1b41
Ivan Espinoza <icaro_19@hotmail.com>
2021-09-07 09:26:02 +0200

    finish work

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ module.exports = app => {
   const baseRoutes = require("./base.routes");
   app.use("/", baseRoutes);
 
+  // Course routes
+  app.use("/cursos", require('./course.router'));
+
+  // Students routes
   const student = require("./student.router");
   app.use("/estudiantes", student);
+
 
   // Auth routes
   const authRoutes = require("./auth.routes");
