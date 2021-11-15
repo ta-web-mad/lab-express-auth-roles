2edc9d46e884ee97d831a43bc90ceada0878a704
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-15 20:42:40 +0100

    Complete mandatory iterations

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
-module.exports = app => {
-  
+module.exports = (app) => {
   // Base routes
-  const baseRoutes = require("./base.routes");
-  app.use("/", baseRoutes);
-  
+  const baseRoutes = require('./base.routes')
+  app.use('/', baseRoutes)
+
   // Auth routes
-  const authRoutes = require("./auth.routes");
-  app.use("/", authRoutes); 
+  const authRoutes = require('./auth.routes')
+  app.use('/', authRoutes)
+
+  // Student routes
+  const studentRoutes = require('./student.routes')
+  app.use('/students', studentRoutes)
 }
