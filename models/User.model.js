c9655f94675f02319c07410c0b723ce226d44873
RaselJam <Rasel.jam.h@gmail.com>
2021-11-17 09:01:16 +0100

    not done

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,25 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const userSchema = new Schema(
-  {
-    username: { type: String, unique: true },
-    name: String,
-    password: String,
-    profileImg: String,
-    description: String
-    // add roles setup here
-  },
-  {
-    timestamps: true
-  }
-);
+	{
+		username: { type: String, unique: true },
+		name: String,
+		password: String,
+		profileImg: String,
+		description: String,
+		// add roles setup here
+		role: {
+       type: String,
+        enum :['STUDENT', 'DEV','TA','PM'],
+        default : 'STUDENT'
+    },
+	},
+	{
+		timestamps: true,
+	}
+)
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
