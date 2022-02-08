f6cc39875d3dfaedc24f189cf8b2a765ec9ce200
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-02-08 23:29:23 +0100

    solved duplication of buttons  when user is PM

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,9 +1,17 @@
 const { logueado, checkRole } = require("../middleware/route-ward")
+const User = require("../models/User.model")
 
 const router = require("express").Router()
 
 router.get('/course',logueado, checkRole('TA'),  (req, res, next) => {
-    res.render('course/create')
+    
+    User
+        .find()
+        .then(users => res.render('course/create', {users}) )
+        .catch(error => next(error))
+
+    
+    
 })
 
 router.post('/course', logueado, checkRole('TA'), (req, res, next) =>{
