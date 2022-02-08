4ebffd4920ff41084b3b70cc67c2bd830cb89480
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-02-08 23:59:50 +0100

    solved iteration to display only TA on create course form

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -3,18 +3,25 @@ const User = require("../models/User.model")
 
 const router = require("express").Router()
 
-router.get('/course',logueado, checkRole('TA'),  (req, res, next) => {
-    
+router.get('/course', logueado, checkRole('TA'), (req, res, next) => {
+    const data = {}
     User
         .find()
-        .then(users => res.render('course/create', {users}) )
+        .then((users) => {
+            data.users = users
+            return User.find({ role: 'TA' })
+        })
+        .then(ta => {
+            data.ta = ta
+            console.log(data)
+            res.render('course/create', { data }) })
         .catch(error => next(error))
 
-    
-    
+
+
 })
 
-router.post('/course', logueado, checkRole('TA'), (req, res, next) =>{
+router.post('/course', logueado, checkRole('TA'), (req, res, next) => {
     res.redirect('https://cutt.ly/AOV18LS')
 })
 
