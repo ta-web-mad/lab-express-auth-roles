0524072b78c0e72fc847a36c8d848fa12756a3ab
yoaomoreira <jpmoreiraes@gmail.com>
2022-11-15 20:16:44 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 module.exports = app => {
-  
+
   // Base routes
   const indexRouter = require("./index.routes");
   app.use("/", indexRouter);
-  
+
   // Auth routes
   const authRouter = require("./auth.routes");
-  app.use("/", authRouter); 
+  app.use("/", authRouter);
+
+  // Students routes
+  const studentsRouter = require("./student.routes");
+  app.use("/", studentsRouter);
 }
+
+
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,28 @@
 const router = require("express").Router()
 
+const { isLoggedIn, checkRoles } = require('../middleware/route-guard')
+
 router.get("/", (req, res, next) => {
   res.render("index")
 })
+
+
+
+
+
+
+
+
+
+
+
+// router.get("/mi-perfil", isLoggedIn, (req, res, next) => {
+//   res.render("user/profile", { user: req.session.currentUser })
+// })
+
+// // admin route: role based access
+// router.get('/admin', isLoggedIn, checkRoles('ADMIN', 'EDITOR'), (req, res, next) => {
+//   res.render('admin-panel')
+// })
 
 module.exports = router
