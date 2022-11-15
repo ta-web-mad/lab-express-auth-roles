aaa1f46cbc75042bd7f597261dadf3b853ce3166
PedroSG00 <suarezgallardopedro@gmail.com>
2022-11-15 20:59:14 +0100

    done bonusless

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,3 +1,22 @@
 function loggedIn(req, res, next) {
     req.session.currentUser ? next() : res.render('auth/login', { errorMessage: 'Log In to access' })
 }
+
+function loggedOut(req, res, next) {
+    !req.session.currentUser ? next() : res.redirect('/')
+}
+
+const checkRoles = (...rolesToCheck) => (req, res, next) => {
+    if (rolesToCheck.includes(req.session.currentUser.role)) {
+        next()
+    } else {
+        res.render('auth/login', { errorMessage: `No tienes permisos de ${roleToCheck}` })
+    }
+}
+
+
+module.exports = {
+    loggedIn,
+    loggedOut,
+    checkRoles,
+}
--- models/User.model.js
+++ models/User.model.js
@@ -4,11 +4,11 @@ const userSchema = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    password: String,
-    profileImg: { type: String, default: 'https://i.stack.imgur.com/l60Hf.png' },
+    password: { type: String, required: true, minLength: 8 },
+    profileImg: { type: String, default: 'https://i.stack.imgur.com/l60Hf.png', required: true },
     description: { type: String, default: 'No existe descripción.' },
     // add roles setup here
-    roles: {
+    role: {
       type: String,
       Enum: ['STUDENT', 'TA', 'DEV', 'PM'],
       default: 'STUDENT'
