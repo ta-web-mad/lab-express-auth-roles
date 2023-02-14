97fefb83cbd30155a7c8ed5837db6b576af2c43d
Andrei Costea <andreicst.97@gmail.com>
2023-02-14 23:32:23 +0100

    [MAD DE 0123]Andrei Costea

--- config/session.config.js
+++ config/session.config.js
@@ -14,10 +14,10 @@ module.exports = app => {
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
-                maxAge: 60000
+                maxAge: 100000000
             },
             store: MongoStore.create({
-                mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/basic-auth'
+                mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ironhack-learn'
             })
         })
     );
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
+const { use } = require("./user.routes")
+
 module.exports = app => {
-  
-  // Base routes
-  const indexRouter = require("./index.routes");
-  app.use("/", indexRouter);
-  
-  // Auth routes
-  const authRouter = require("./auth.routes");
-  app.use("/", authRouter); 
+
+  const indexRouter = require("./index.routes")
+  app.use("/", indexRouter)
+
+  const authRouter = require("./auth.routes")
+  app.use("/", authRouter)
+
+  const userRouter = require('./user.routes')
+  app.use('/students', userRouter)
 }
