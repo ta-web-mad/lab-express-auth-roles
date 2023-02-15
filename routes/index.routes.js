56f6d7c56e1269aa4a6546aa1cbebe01047ca9a8
danisalo <your_github_email>
2023-02-15 10:15:12 +0100

    done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,27 @@
 const router = require("express").Router()
+const { isLoggedIn, isLoggedOut, checkRole, checkUser } = require('../middlewares/route-guard')
+
+// Model
+const User = require("../models/User.model")
 
 router.get("/", (req, res, next) => {
   res.render("index")
+})
+
+// List Staff
+router.get("/staff", isLoggedIn, (req, res, next) => {
+  User
+    .find({
+      $or: [
+        { role: 'TA' },
+        { role: 'DEV' },
+        { role: 'PM' }
+      ]
+    })
+    .then(users => {
+      res.render("staff", { users })
+    })
+    .catch(err => next(err))
 })
 
 module.exports = router
