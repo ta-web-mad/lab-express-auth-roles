0d1f68d378a33333b3b6fa7bb6738e729b6dfabc
Jon <jon_ander_23@protonmail.com>
2023-04-22 13:17:03 +0200

    FirstIteration

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
-module.exports = app => {
-  
+module.exports = (app) => {
   // Base routes
   const indexRouter = require("./index.routes");
   app.use("/", indexRouter);
-  
+
   // Auth routes
   const authRouter = require("./auth.routes");
-  app.use("/", authRouter); 
-}
+  app.use("/", authRouter);
+};
