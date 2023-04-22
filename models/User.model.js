b4109e79108e6208c315f8a0f202e8fc61a9d630
raqueldfrutos <raqueldfrutos@gmail.com>
2023-04-22 11:25:42 +0200

    Roles created in model

--- models/User.model.js
+++ models/User.model.js
@@ -4,10 +4,11 @@ const userSchema = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    password: String,
+    password:{ type:String, required:true,  match: [/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/, 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.']},
     profileImg: { type: String, default: 'https://i.stack.imgur.com/l60Hf.png' },
-    description: { type: String, default: 'No existe descripción.' }
+    description: { type: String, default: 'No existe descripción.' },
     // add roles setup here
+    roles: { type: String, enum: ["STUDENT", "DEV", "TA","PM"], default: 'STUDENT'}
   },
   {
     timestamps: true
