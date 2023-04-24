77ebdf3022f108b7265a623f0acc190c5ed521e5
pepealacid <ppalacid@gmail.com>
2023-04-24 14:52:45 +0200

    bonus done

--- models/User.model.js
+++ models/User.model.js
@@ -4,17 +4,32 @@ const userSchema = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    password: String,
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^(?=.*\d).{8,}$/.test(v);
+        },
+        message: "Password must have at least 8 characters and 1 number",
+      },
+    },
     role: {
       type: String,
       enum: ["STUDENT", "DEV", "TA", "PM"],
       default: "STUDENT",
+      required: true,
     },
     profileImg: {
       type: String,
       default: "https://i.stack.imgur.com/l60Hf.png",
+      required: true,
     },
-    description: { type: String, default: "No existe descripción." },
+    description: {
+      type: String,
+      default: "No existe descripción.",
+      required: true,
+    },
     // add roles setup here
   },
   {
--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -5,8 +5,17 @@ const User = require("../models/User.model");
 
 router.get("/create", checkRole("TA"), async (req, res, next) => {
   try {
-    const dbUsers = User.findById().populate("username");
+    const dbUsers = await User.find();
+    console.log(`db Users: ${dbUsers}`);
     res.render("courses/create-course", { dbUsers });
+  } catch (error) {
+    res.render("error", { error });
+  }
+});
+
+router.post("/create", checkRole("TA"), async (req, res, next) => {
+  try {
+    await Course.create(req.body);
   } catch (error) {
     res.render("error", { error });
   }
