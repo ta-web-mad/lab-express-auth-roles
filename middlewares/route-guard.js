ab1ddcaa120aa3023c228f8b1b42847572524115
Manuel Pérez <mperezpradosjose@gmail.com>
2023-05-09 22:02:54 +0200

    Realizado asta la mitad de el primer bonus, me parecio muy interesante el lab y bastante bueno para afianzar los conocimientos de este segundo modulo

---
+++ middlewares/route-guard.js
@@ -0,0 +1,31 @@
+const isLoggedIn = (req, res, next) => {
+    req.session.currentUser ? next() : res.render('auth/login', { errorMessage: 'Inicia sesión para continuar' })
+}
+
+const isLoggedOut = (req, res, next) => {
+    !req.session.currentUser ? next() : res.redirect('/libros/listado')
+}
+
+const checkRoles = (...admittedRoles) => (req, res, next) => {
+
+    const isAdmitted = admittedRoles.includes(req.session.currentUser.role)
+
+    if (isAdmitted) {
+        next()
+    } else {
+        res.render('auth/login', { errorMessage: 'Acceso no autorizado' })
+    }
+}
+
+const checkUser = (req, res, next) => {
+    const { _id } = req.params
+    if (_id === req.session.currentUser._id || req.session.currentUser.role === 'PM') {
+        next()
+    } else {
+        res.render('auth/login', { errorMessage: 'Acceso no autorizado' })
+    }
+}
+
+
+
+module.exports = { isLoggedIn, isLoggedOut, checkRoles, checkUser }
